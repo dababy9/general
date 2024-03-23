2c023cb1e3f181d7c128d239bed9aaa354e7ee6a
Caleb Walker <caleb.walker62@gmail.com>
2024-03-23 15:45:09 -0400

    .

--- usna/it430/proj/test_part1.py
+++
@@ -1,9 +0,0 @@
-# test_part1.py
-
-from project import *
-
-key = bytes.fromhex("00112233445566778899aabbccddeeff")
-ecb = bytes.fromhex("000102030405060708090a0b0c0d0e0f")
-encrypt_ecb("pic_original.bmp", "prj_ecb.bin", key)
-decrypt_ecb("prj_ecb.bin", "prj_dec.bmp", key)
-fix_bmp_header("pic_original.bmp", "prj_ecb.bin", "prj_ecb.bmp")
