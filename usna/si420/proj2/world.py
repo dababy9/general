24d38b32434beb03850a0d73299c585b3c9bb240
Caleb Walker <caleb.walker62@gmail.com>
2024-03-24 00:53:54 -0400

    .

--- usna/si420/proj2/world.py
+++ usna/si420/proj2/world.py
@@ -1,11 +1,55 @@
 import json
-
-
+import copy
 
 class World:
 
     def __init__(self, fName):
         with open(fName, 'r') as filehandle:
-            self.dict = json.load(filehandle)
+            d = json.load(filehandle)
+        self.rows, self.cols = d["shape"][0], d["shape"][1]
+        self.g = d["gamma"]
+        self.rl = {}
+        for s in d["rl"]:
+            self.rl[self.__hash(s[0])] = s[1]
+        self.tl, self.bl = d["tl"], d["bl"]
+        self.values = [[0] * self.cols for _ in range(self.rows)]
+        self.policy = [['N'] * self.cols for _ in range(self.rows)]
+        for s in d["tl"] + d["bl"]:
+            self.policy[s[0]][s[1]] = '.'
+    
+    def valueIteration(self, n):
+        for _ in range(n):
+            newValue = copy.deepcopy(self.values)
+            for r in range(self.rows):
+                for c in range(self.cols):
+                    if [r, c] not in self.bl and [r, c] not in self.tl: [self.policy[r][c], newValue[r][c]] = self.__update([r, c])
+            self.values = newValue
 
+    def __update(self, square):
+        aList = {'N': 0, 'S': 0, 'E': 0, 'W': 0}
+        for a in aList.keys():
+            aList[a] = self.__qStar(a, square)
+        maxA = ['N', aList['N']]
+        for a in aList.keys():
+            if aList[a] > maxA[1]: maxA = [a, aList[a]]
+        return maxA
+    
+    def __qStar(self, a, square):
+        others = ('N', 'S') if a == 'E' or a == 'W' else ('E', 'W')
+        return 0.8 * self.__tReward(self.__move(square, a)) + 0.1 * self.__tReward(self.__move(square, others[0])) + 0.1 * self.__tReward(self.__move(square, others[1]))
+    
+    def __tReward(self, square):
+        reward = self.rl[self.__hash(square)] if self.__hash(square) in self.rl else 0
+        return reward + self.g * self.values[square[0]][square[1]]
+    
+    def __move(self, square, a):
+        result = square
+        if a == 'N': result = [square[0]-1, square[1]]
+        if a == 'S': result = [square[0]+1, square[1]]
+        if a == 'E': result = [square[0], square[1]-1]
+        if a == 'W': result = [square[0], square[1]+1]
+        return result if result[0] >= 0 and result[0] < self.rows and result[1] >= 0 and result[1] < self.cols else square
+            
+    def __hash(self, p):
+        return p[0] * self.cols + p[1]
     
