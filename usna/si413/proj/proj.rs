a4b7ab3e3ea27b36d966c2aebbea5f5a2e1b83e0
dababy9 <m256642@usna.edu>
2024-09-17 09:45:10 -0400

    .

--- usna/si413/proj/proj.rs
+++ usna/si413/proj/proj.rs
@@ -1,5 +1,5 @@
 trait Lyrics {
-    fn bottles(&self, _: bool) -> Self;
+    fn bottles(&self, bp: str, wp: str) -> Self;
     fn take(&self) -> Self;
     fn wall(&self) -> Self;
     fn mid(&self) -> Self;
@@ -7,20 +7,14 @@ trait Lyrics {
 }
 
 impl Lyrics for u32 {
-    fn bottles(&self, cap:bool) -> u32 {
-        match *self {
-            0 => print!("{}o more bottles of beer", if cap {"N"} else {"n"}),
-            1 => print!("{} bottle of beer", self),
-            _ => print!("{} bottles of beer", self)
-        }
+    fn bottles(&self) -> u32 {
+        print!("{0}{1}{2}{3}", self, if *self == 1 {""} else {"s"});
         *self
     }
 
     fn take(&self) -> u32 {
-        match *self {
-            0 => { print!("Go to the store and buy some more, "); 99 }
-            _ => { print!("Take one down and pass it around, "); *self - 1 }
-        }
+        print!("Take one down and pass it around, ");
+        *self + 1
     }
 
     fn wall(&self) -> u32 {
@@ -38,10 +32,16 @@ impl Lyrics for u32 {
     }
 }
 
-fn main() {
-    for i in (0..100).rev() {
-        i.bottles(true).wall().mid().bottles(false).end();
-        i.take().bottles(false).wall().end();
+fn part_b(){
+    bottlePhrase = "line";
+    wallPhrase = "of text on the screen, "
+    for i in 1..100 {
+        i.bottles().wall().mid().bottles().end();
+        i.take().bottles().wall().end();
         println!();
     }
+}
+
+fn main() {
+    part_b();
 }
