acfc76d33a5f00ed5ea26cf90b7f87aeae847f80
Caleb Walker <caleb.walker62@gmail.com>
2024-10-21 20:48:18 -0400

    .

--- usna/ic470/server.py
+++ usna/ic470/server.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+from spotipy.oauth2 import SpotifyOAuth
+import spotipy 
 import uvicorn
 import socket
-from pydantic import BaseModel
-
-import spotipy 
-from spotipy.oauth2 import SpotifyOAuth
-from pprint import pprint
-import random
+import asyncio
 
 # create app
 app = FastAPI()
@@ -16,11 +14,15 @@ app = FastAPI()
 class Text(BaseModel):
     text: str
 
+
+
 # define root endpoint (serve html page)
 @app.get("/")
 async def home():
     f = open("index.html")
     return HTMLResponse(content=f.read(), status_code=200)
+
+
 
 # define request endpoint (user requesting to add song to queue)
 @app.post("/request")
@@ -34,6 +36,9 @@ async def store_song(body: Text):
         if uri != "":
             sp.add_to_queue(uri, None)
 
+        # wait briefly to make sure the song is actually added
+        await asyncio.sleep(0.15)
+
         # retrieve queue (song name and artist name) and return it to user
         result = sp.queue()
         queue = []
@@ -46,6 +51,8 @@ async def store_song(body: Text):
     except Exception as error:
         print(error)
         return {"message": "NOT OK"}
+
+
 
 # define search endpoint (user searching for song)
 @app.post("/search")
@@ -67,6 +74,8 @@ async def search_song(body: Text):
         print(error)
         return {"message": "NOT OK"}
 
+
+
 # define main method to run server
 if __name__ == "__main__":
     hostname = socket.gethostname()
@@ -75,5 +84,4 @@ if __name__ == "__main__":
                                                client_secret="48bc19fb38e1452cb7c90b9a40eb9464",
                                                redirect_uri="http://localhost:9000",
                                                scope="user-library-read,user-read-recently-played,user-read-currently-playing,user-read-playback-state,user-modify-playback-state"))
-    uvicorn.run(app, host="127.0.0.1", port=8080)
-    
+    uvicorn.run(app, host="0.0.0.0", port=8888)
