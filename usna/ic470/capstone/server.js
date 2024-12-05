e0c39fff8383d44f3c5c1dbd3d751e6af4db444f
Caleb <caleb.walker62@gmail.com>
2024-12-04 23:25:22 -0500

    .

--- usna/ic470/capstone/server.js
+++ usna/ic470/capstone/server.js
@@ -1,12 +1,32 @@
+// Constants pertinent to server functionality
+const port = 9000;
+
+// Import necessary modules/methods
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const { Game } = require('./game.js');
+
+// Create Express app and pass it to HTTP server
 const app = express();
-const http = require('http');
-const server = http.createServer();
+const server = createServer(app);
 
+// Mount the socket.io server on the HTTP server
+const io = new Server(server);
+
+// Express app now serves static files from 'static' directory
+app.use('/static', express.static('static'));
+
+// ---------- DEFINING ENDPOINTS ----------
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/html/index.html');
 });
 
-server.listen(9000, () => {
-    console.log('listening on port 9000');
+// Bind server to the specified port, and listen for incoming requests
+server.listen(port, () => {
+    console.log('Listening on port 9000');
 });
+
+io.on('connection', (socket) => {
+    console.log("connec");
+})
---
+++ usna/ic470/capstone/static/scripts/script.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    alert('hi');
+});
