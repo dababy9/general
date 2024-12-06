03bf176993949a6b11fc1792c111e747827bb807
dababy9 <m256642@usna.edu>
2024-12-06 09:01:15 -0500

    .

--- usna/si413/lab12/src/main/java/si413/spl/ast/NewClass.java
+++ usna/si413/lab12/src/main/java/si413/spl/ast/NewClass.java
@@ -17,7 +17,13 @@ public class NewClass extends Expression {
     public Value evaluate(Frame env) {
         ClassClosure cc = lhs.evaluate(env).getCC();
         if(cc == null) return Value.unset();
-        Frame objFrame = new Frame(cc.getEnv());
+        Frame superEnv = cc.getSuperEnv();
+        if(superEnv != null){
+            for(Statement s : cc.getSuperCls().getBody().getChildren()){
+                s.execute(superEnv);
+            }
+        }
+        Frame objFrame = new Frame(cc.getEnv(), superEnv);
         for(Statement s : cc.getCls().getBody().getChildren()){
             s.execute(objFrame);
         }
