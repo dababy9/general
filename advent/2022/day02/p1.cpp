1e262a403d3cb6b53054f201c93b3d3c5aeec731
dababy9 <caleb.walker62@gmail.com>
2025-12-14 18:43:52 -0500

    .

--- advent/2022/day02/p1.cpp
+++ advent/2022/day02/p1.cpp
@@ -2,29 +2,14 @@
 #include <iostream>
 using namespace std;
 
-int win(char, char);
-
 int main(){
-  ifstream fin("input.txt");
-  char them, me;
-  int total = 0;
-  while(fin >> them >> me){
-    total += win(them, me);
-    if(me == 'X')
-      total++;
-    else
-      total += me == 'Y' ? 2 : 3;
-  }
-  cout << total << endl;
-  return 0;
-}
-
-int win(char a, char b){
-  if(a == b-23)
-    return 3;
-  if(a == 'A')
-    return b == 'Y' ? 6 : 0;
-  if(a == 'B')
-    return b == 'Z' ? 6 : 0;
-  return b == 'X' ? 6 : 0;
+    ifstream fin("input.txt");
+    char aChar, bChar;
+    int total = 0;
+    while (fin >> aChar >> bChar){
+        int a = aChar - 'A', b = bChar - 'X';
+        total += b + 1 + ((b - a) % 3 + 4) % 3 * 3;
+    }
+    cout << total << endl;
+    return 0;
 }
--- advent/2022/day02/p2.cpp
+++ advent/2022/day02/p2.cpp
@@ -2,37 +2,14 @@
 #include <iostream>
 using namespace std;
 
-char myplay(char, char);
-
 int main(){
-  ifstream fin("input.txt");
-  char them, outcome;
-  int total = 0;
-  while(fin >> them >> outcome){
-    if(outcome == 'Y')
-      total += 3;
-    if(outcome == 'Z')
-      total += 6;
-    char me = myplay(them, outcome);
-    total += me-'A'+1;
-  }
-  cout << total << endl;
-  return 0;
-}
-
-char myplay(char them, char outcome){
-  if(outcome == 'Y')
-    return them;
-  if(outcome == 'X'){
-    if(them == 'A')
-      return 'C';
-    if(them == 'B')
-      return 'A';
-    return 'B';
-  }
-  if(them == 'A')
-    return 'B';
-  if(them == 'B')
-    return 'C';
-  return 'A';
+    ifstream fin("input.txt");
+    char aChar, bChar;
+    int total = 0;
+    while (fin >> aChar >> bChar){
+        int a = aChar - 'A', b = bChar - 'X';
+        total += b + 1 + ((b - a) % 3 + 4) % 3 * 3;
+    }
+    cout << total << endl;
+    return 0;
 }
